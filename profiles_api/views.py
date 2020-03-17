49e4e903d78d54970257955fb858152d8157a2c5
Mahmudul Alam <expelmahmud@gmail.com>
2020-03-17 20:58:28 +0600

    list, create, retrieve, update, partial_update, destroy method added into viewsets

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -67,3 +67,41 @@ class HelloViewset(ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+
+    def create(self, request):
+        """Create a new hello message."""
+
+        serializer = HelloSerializer
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its ID."""
+
+        serializer = HelloSerializer(data=request.data)
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handles removing an object"""
+
+        return Response({'http_method': 'DELETE'})
+
+    
+       
